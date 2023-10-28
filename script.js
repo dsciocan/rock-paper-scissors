1867090eaa72d9b7f0357a9ce1047530882015d9
karok <karola9790@gmail.com>
2023-10-28 16:04:12 +0100

    finished game

--- script.js
+++ script.js
@@ -1,63 +1,57 @@
-// 1. Initialize `choices` array: r, p, s. representing rock, paper, or scissors.
-var choices = ["r","p","s"];
-// 5. We then add to their score.
+// Create variables for three different options: R, P and S
+var choices = [
+    'R', 
+    'P', 
+    'S'
+];
 
-//    * If user wins, then we add one to their wins.
-var userWin = 0;
-//    * If user loses, then we add one to their losses.
-    var userLoss = 0;
-//    * If user ties, then we add one to their ties.
-    var tie = 0;
-// 2. Prompt user to enter "r," "p," or "s".
-while (input !== "no") {
-var userChoice = prompt("Choose between r, p and s.");
-console.log(userChoice)
-// 3. Computer chooses a **random** value in a list of "r," "p," or "s."
-var computerChoice = choices[Math.floor(Math.random()*choices.length)];
-console.log(computerChoice);
+// Create variables for wins, loses, or ties
+var wins = 0;
+var loses = 0;
+var ties = 0;
 
-// 4. We determine if the user wins or not.
 
-//    * Initialize `wins`, `losses`, `ties` variables to 0
-
-//    * If user picks rock and if computer picks scissors, then user wins.
-if (userChoice == "r" && computerChoice == "s") {
-    alert("The opponent has picked " + computerChoice + ". You won!");
-    userWin++;
-} else if (userChoice == "r" && computerChoice == "p") {
-//    * If user picks rock and if computer picks paper, then user loses.
-    alert("The opponent has picked " + computerChoice + ". You lost :(");
-    userLoss++;
-} else if (userChoice == "s" && computerChoice == "r") {
-//    * If user picks scissors and if computer picks rock, then user loses.
-    alert("The opponent has picked " + computerChoice + ". You lost :(");
-    userLoss++;
-} else if (userChoice == "s" && computerChoice == "p") {
-//    * If user picks scissors and if computer picks paper, then user wins.
-    alert("The opponent has picked " + computerChoice + ". You won!");
-    userWin++;
-} else if (userChoice == "p" && computerChoice == "r") {
-//    * If user picks paper and if computer picks rock, then user wins.
-    alert("The opponent has picked " + computerChoice + ". You won!");
-    userWin++;
-} else if (userChoice == "p" && computerChoice == "s") {
-//    * If user picks paper and if computer picks scissors, then user loses.
-    alert("The opponent has picked " + computerChoice + ". You lost :(");
-    userLoss++;
-//    * If user picks the same as computer, then they tie.
-} else if(userChoice == computerChoice) {
-    alert("The opponent has picked " + computerChoice + ". Tie.");
-    tie++;
-}
-// 5. We then add to their score.
-alert ("You have " + userWin + " wins, " + userLoss + " losses, and " + tie + " ties.");
-//    * If user wins, then we add one to their wins.
+for (var i = 0; i < 10; i++) {
+// Prompt user to choose between R, P and S
+    var userChoice = prompt('Please choose between "R", "P", and "S" (use capital letters).');
     
-//    * If user loses, then we add one to their losses.
+    // Create a variable for random computer selection between R, P and S
+    var computerChoice = choices[Math.floor(Math.random()*choices.length)];
     
-//    * If user ties, then we add one to their ties.
-var input = prompt("Would you like to play again? Pick yes or no.");
-}
-// 6. Prompt user to play again.
+        // If else statements for different options
+        // Alert user of the computer/opponent choice and of the result: "You won/lost or Tie"
+        if (userChoice === "R" && computerChoice === "R") {
+            alert("Your opponent chose " + computerChoice + ". Tie.");
+            ties++;
+        } else if (userChoice === "R" && computerChoice === "P") {
+            alert("Your opponent chose " + computerChoice + ". YOU LOST :(");
+            loses++;
+        } else if (userChoice === "R" && computerChoice === "S") {
+            alert("Your opponent chose " + computerChoice + ". YOU WON! :)");
+            wins++;
+        } else if (userChoice === "P" && computerChoice === "P") {
+            alert("Your opponent chose " + computerChoice + ". Tie.");
+            ties++;
+        } else if (userChoice === "P" && computerChoice === "R") {
+            alert("Your opponent chose " + computerChoice + ". YOU WON! :)");
+            wins++;
+        } else if (userChoice === "P" && computerChoice === "S") {
+            alert("Your opponent chose " + computerChoice + ". YOU LOST :(");
+            loses++;
+        } else if (userChoice === "S" && computerChoice === "S") {
+            alert("Your opponent chose " + computerChoice + ". Tie.");
+            ties++;
+        } else if (userChoice === "S" && computerChoice === "P") {
+            alert("Your opponent chose " + computerChoice + ". YOU WON! :)");
+            wins++;
+        } else if (userChoice === "S" && computerChoice === "R") {
+            alert("Your opponent chose " + computerChoice + ". YOU LOST :(");
+            loses++;
+        // If different choice alert: "WRONG CHOICE. GAME OVER :("
+        } else {
+            alert('WRONG CHOICE. GAME OVER :(');
+        } 
+    }
 
-
+// Alert user of total wins, ties and loses after 10 rounds
+alert("You won " + wins + " times, lost " + loses + " times and tied " + ties + " times. Refresh the page to play again");
